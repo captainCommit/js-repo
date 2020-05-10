82b6307a4dcc14132fc14d9f45da34ae5d228c26
Suparno1998 <ssuparno1998@gmail.com>
2020-05-10 10:40:20 +0000

    Stage-3 Add Appointment Modal along with snackbar

--- src/pages/material.js
+++ src/pages/material.js
@@ -65,50 +65,21 @@ const headCells = [
   { id: 'doctor', numeric: false, disablePadding: false, label: 'Doctor Alloted' },
 ];
 
-function EnhancedTableHead(props) {
-  const {order, orderBy, onRequestSort } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
-
+function EnhancedTableHead() {
   return (
     <TableHead>
       <TableRow>
         <TableCell padding="checkbox">
         </TableCell>
         {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align="left"
-            padding={headCell.disablePadding ? 'none' : 'default'}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
+          <TableCell key={headCell.id} align="left" padding={headCell.disablePadding ? 'none' : 'default'}>
               {headCell.label}
-              {orderBy === headCell.id ? (
-                <span style={{border: 0,clip: 'rect(0 0 0 0)',height: 1,margin: -1,overflow: 'hidden',padding: 0,position: 'absolute',top: 20,width: 1,}}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-              ) : null}
-            </TableSortLabel>
           </TableCell>
         ))}
       </TableRow>
     </TableHead>
   );
 }
-
-EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
 
 const useToolbarStyles = makeStyles((theme) => ({
   root: {
@@ -174,29 +145,18 @@ export default class EnhancedTable extends Component{
         super(props)
         this.state = {
           app : Appointments,
-          order:'asc',
-          orderBy:'name',
           selected:"",
-          page:0,
-          rowsPerPage:5,
-          emptyRows:0,
           open:false,
           openAdd:false,
           selectedObject : null,
           alert : false,
           alertType : null,
           alertMessage : null,
-        }  
-        this.setOrder = this.setOrder.bind(this)  
+        }   
         this.snackClose = this.snackClose.bind(this)  
         this.handleDelete = this.handleDelete.bind(this)
         this.handleUpdate = this.handleUpdate.bind(this)  
-        this.setOrderBy = this.setOrderBy.bind(this)
         this.setSelected = this.setSelected.bind(this)
-        this.setPage = this.setPage.bind(this)
-        this.setRowsPerPage = this.setRowsPerPage.bind(this)
-        this.setEmptyRows = this.setEmptyRows.bind(this)
-        this.handleRequestSort = this.handleRequestSort.bind(this)
         this.isSelected = this.isSelected.bind(this)
         this.handleClick = this.handleClick.bind(this)
         this.handleOpen = this.handleOpen.bind(this)
@@ -219,29 +179,9 @@ export default class EnhancedTable extends Component{
     handleClose = ()=>{
         this.setState({open : false})
     }
-    setOrder = (o)=>{
-        this.setState({order:o})
-    }
-    setOrderBy = (o)=>{
-        this.setState({orderBy:o})
-    }
     setSelected = (s)=>{
         this.setState({selected : s})
     }
-    setPage = (p)=>{
-        this.setState({page : p})
-    }
-    setRowsPerPage = (r)=>{
-        this.setState({rowsPerPage : r})
-    }
-    setEmptyRows = (e)=>{
-        this.setState({emptyRows : this.state.rowsPerPage - Math.min(this.state.rowsPerPage, this.state.app.length - (this.state.page * this.state.rowsPerPage))})
-    }
-    handleRequestSort = (event, property) => {
-        const isAsc = this.state.orderBy === property && this.state.order === 'asc';
-        this.setOrder(isAsc ? 'desc' : 'asc');
-        this.setOrderBy(property);
-    };
     handleClick = (evt,name)=>{
         if(name === this.state.selected)
             this.setState({selected : "",selectedObject:null})
@@ -297,9 +237,9 @@ export default class EnhancedTable extends Component{
                 <EnhancedTableToolbar numSelected={this.state.selected === "" ? 0:1} delete={this.handleDelete} update={this.handleOpen}/>
                 <TableContainer>
                     <Table style={{minWidth : 750}} aria-labelledby="tableTitle" size={'medium'} aria-label="enhanced table">
-                        <EnhancedTableHead numSelected={this.state.selected === ""?0:1} order={this.state.order} orderBy={this.state.orderBy} onRequestSort={this.handleRequestSort} rowCount={this.state.app.length} />
+                        <EnhancedTableHead/>
                         <TableBody>
-                            {stableSort(this.state.app, getComparator(this.state.order, this.state.orderBy)).map((row, index) => {const isItemSelected = this.isSelected(row.name);const labelId = `enhanced-table-checkbox-${index}`;
+                            {this.state.app.map((row, index) => {const isItemSelected = this.isSelected(row.name);const labelId = `enhanced-table-checkbox-${index}`;
                                 return (
                                     <TableRow hover onClick={(event) => this.handleClick(event, row.name)} role="checkbox" aria-checked={isItemSelected} tabIndex={-1} key={row.name} selected={isItemSelected} >
                                         <TableCell padding="checkbox"><Checkbox checked={isItemSelected} inputProps={{ 'aria-labelledby': labelId }} /></TableCell>
